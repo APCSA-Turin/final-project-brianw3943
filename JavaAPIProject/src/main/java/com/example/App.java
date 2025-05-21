9104e0c099b91f4f10635c472cb3bedb73f83965
brianw3943 <brianw124@nycstudents.net>
2025-05-21 16:30:37 +0000

    commit

--- JavaAPIProject/src/main/java/com/example/App.java
+++ JavaAPIProject/src/main/java/com/example/App.java
@@ -1,7 +1,10 @@
 package com.example;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
@@ -13,21 +16,34 @@ public class App extends JPanel implements ActionListener
     private Image sprite;
     private Timer timer;
     JButton button;
+    JButton start;
     JLabel label;
     public App() throws Exception {
+        setLayout(null);
         button = new JButton();
-        button.addActionListener(this);
-        add(button);
         button.setPreferredSize(new Dimension(30, 30));
         button.setOpaque(false);
         button.setContentAreaFilled(false);
         button.setBorderPainted(false);
-        label = new JLabel("Score: 0", JLabel.LEFT);
-        add(label);
-        label.setBounds(10,10,50,20);
-        sprite = new ImageIcon("").getImage();
+        button.setLayout(null);
+        button.setBounds(0, 100, 30, 30);
+        this.add(button);
+        start = new JButton("Start");
+        start.setPreferredSize(new Dimension(100, 30));
+        start.setBounds(900, 0, 100, 30);
+        start.addActionListener(this);
+        this.add(start);
+        label = new JLabel("Score: " + count);
+        this.add(label);
+        label.setPreferredSize(new Dimension(50, 20));
+        label.setBounds(10,10,100,20);
+        URL url = new URL("https://api.thecatapi.com/v1/images/search");
+        BufferedImage image = ImageIO.read(url);
+        ImageIcon icon = new ImageIcon(image);
+        sprite = icon.getImage();
         timer = new Timer(20, this);
         timer.start();
+        
     }
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -41,13 +57,20 @@ public class App extends JPanel implements ActionListener
         if (e.getSource() == button) {
             label.setText("Score: " + count);
             count++;
-            int random = (int) (Math.random() * 1800) + 100;
-            int random2 = (int) (Math.random() * 950) + 50;
+            int random = (int) (Math.random() * 1760) + 100;
+            int random2 = (int) (Math.random() * 920) + 50;
             position[0] = random;
             position[1] = random2;
             Dimension size = button.getPreferredSize();
             button.setBounds(random, random2, size.width, size.height);
             repaint();
+        } else if (e.getSource() == start) {
+            count++;
+            start.setVisible(false);
+            start.removeActionListener(this);
+            button.addActionListener(this);
+        } else {
+            
         }
     }
     public static void main( String[] args ) throws Exception
